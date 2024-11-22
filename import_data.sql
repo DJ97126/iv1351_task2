4adc13dfc4269fcda6a4bb3b392a271c1d7de5bd
Justin <124042841+DJ97126@users.noreply.github.com>
2024-11-22 05:34:47 +0100

    sql code for importing to the db
    it is not working though

---
+++ import_data.sql
@@ -0,0 +1,31 @@
+INSERT INTO person (name, mail_address, address, personal_number)
+VALUES 	('{
+			"name": "Danielle Richards",
+			"mail_address": "daniellerichards5240@gmail.com",
+			"address": "Värnamo",
+			"personal_number": "40D4D1C1-FD27-DA34-6288-81BDED34EBA4"
+		}'::jsonb),
+		('{
+			"name": "Reuben Prince",
+			"mail_address": "reubenprince@gmail.com",
+			"address": "Göteborg",
+			"personal_number": "1C9E0E49-DA58-2DFB-08ED-C2BCA6BE78A3"
+		}'::jsonb),
+		('{
+			"name": "Kuame Kidd",
+			"mail_address": "kuamekidd@gmail.com",
+			"address": "Jönköping",
+			"personal_number": "1E79A32B-3855-2735-A396-ADC7DADD757E"
+		}'::jsonb),
+		('{
+			"name": "Ursa Matthews",
+			"mail_address": "ursamatthews@gmail.com",
+			"address": "Alingsås",
+			"personal_number": "D3B6E522-9B43-C698-8ECA-5715CBFD6D1B"
+		}'::jsonb),
+		('{
+			"name": "Irene Pugh",
+			"mail_address": "irenepugh5672@gmail.com",
+			"address": "Värnamo",
+			"personal_number": "D26D2488-9760-5ADE-5A54-C9EEDD67BB55"
+		}'::jsonb);
