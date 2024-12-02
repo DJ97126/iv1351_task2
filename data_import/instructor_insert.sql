952949c6f80da3de16946919500c1080171e5eb3
Daniela Márquez <126365275+DanyMquez@users.noreply.github.com>
2024-12-02 13:03:31 -0600

    Update instructor_insert.sql

--- data_import/instructor_insert.sql
+++ data_import/instructor_insert.sql
@@ -1,8 +1,7 @@
 INSERT INTO instructor (personal_number, first_name, last_name, address, phone_number, mail_address, person_id)
-VALUES 	('200405436302',
-		'Neville',
-		'Oneil',
-		'Ap #740-5731 Pretium St.',
-		'03 60 23 81 48',
-		'n.oneil@protonmail.ca',
-		27)
+VALUES 	
+	('200405436302', 'Neville', 'Oneil', 'Ap #740-5731 Pretium St.', '03 60 23 81 48', 'n.oneil@protonmail.ca', '27'),
+	('201402428634', 'Felix', 'Dale', 'P.O. Box 764, 3563 Phasellus Rd.', '04 53 57 69 65', 'felix_dale@outlook.ca', '21'),
+	('200413930267', 'Yoko', 'Rosario', '8581 Augue Rd.', '04 57 77 30 16', 'y.rosario@aol.couk', '23'),
+	('200938422237', 'Olivia', 'Page', '624-4973 Eu Av.', '02 22 15 04 52', 'o_page@yahoo.com', '24'),
+	('15468586-3', 'Nissim', 'Stevens', '862-9552 In Av.', '(026) 9620 4123', 'sit.amet.consectetuer@hotmail.couk', '29');
