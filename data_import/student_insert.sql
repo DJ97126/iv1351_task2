a716aba200427a58791cc3f4560b16e5b5846956
Daniela Márquez <126365275+DanyMquez@users.noreply.github.com>
2024-12-02 13:18:22 -0600

    Create student_insert.sql

---
+++ data_import/student_insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO student (personal_number, first_name, last_name, address, mail_address, phone_number, student_id, person_id, contact_person_id, family_id)
+VALUES
+('3390776-1', 'Celeste', 'Chavez', '224-937 Sociis St.', 'curabitur@protonmail.com', '0800 1111', 'O86185414', '4', '22', 'C87270286'),
+('37825696-8', 'Ivory', 'Holcomb', '9342 Dui Street', 'aenean.euismod@outlook.net', '070 5102 4767', 'Y31271106', '5', '23', 'H41030591'),
+('25465913-4','Eaton','Russo','Ap #656-2352 Quam. Av.','fusce@outlook.net','07624 412118','M64686772', '28', '24','U65535834'),
+('15468586-3','Nissim','Stevens','862-9552 In Av.','sit.amet.consectetuer@hotmail.couk','(026) 9620 4123','H41030591', '29', '25', 'X14629112'),
+('16203206-2','Rebecca','Rich','P.O. Box 659, 9145 Iaculis Ave','egestas.urna.justo@protonmail.org','0947 689 4745','Q56258366','30', '26', 'Q84305096'),
+('13383694-2','Ina','Rich','P.O. Box 283, 3444 Phasellus Road','nunc.ac.mattis@aol.couk','055 8342 5013','P12316611', '31', '26', 'Q84305096’),
+('22107509-9','Amela','Hoffman','Ap #831-1378 Ipsum. Avenue','mauris.eu.turpis@outlook.couk','055 0203 1671','O72752983', '32', '27', 'F12488421'),
+('284264-5','Kibo','Hoffman','Ap #831-1378 Ipsum. Avenue','massa.lobortis.ultrices@aol.couk','0979 190 4504', 'K21869353', '33', '27', 'F12488421'),
+('21140861-8','Lillian','Hoffman','Ap #831-1378 Ipsum. Avenue','in@icloud.com','056 1443 5239','R07135186', '34', '27', 'F12488421'),
+('236214-7','Guy','Russo','Ap #288-2861 Feugiat Avenue','commodo@hotmail.net','0956 475 6391','B58668226', '36', '24', 'U65535834'),
+('1982962-6','Kirsten','Russo','966-9532 Blandit Avenue','iaculis.lacus.pede@aol.ca','(01298) 517223','E65663231', '38', '24', 'U65535834'),
+('26868234-1','Rhea','Hoffman','Ap #831-1378 Ipsum. Avenue','elit@aol.com','055 8946 2128', 'D03566811', '39', '27', 'F12488421');
